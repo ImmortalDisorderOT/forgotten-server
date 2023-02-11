2ec1c6c93c8f280514b614bb9a619e52b961eb98
Avaji <103899149+AvajiOT@users.noreply.github.com>
2023-02-11 15:46:45 -0800

    Update createEpicMonster.lua

--- data/scripts/talkactions/adminCommands/createEpicMonster.lua
+++ data/scripts/talkactions/adminCommands/createEpicMonster.lua
@@ -16,6 +16,8 @@ function talk.onSay(player, words, param)
 		monster:getPosition():sendMagicEffect(CONST_ME_TELEPORT)
 		position:sendMagicEffect(CONST_ME_MAGIC_RED)
 		monster:setSkull(SKULL_BLACK)
+		monster:setMaxHealth(monster:getMaxHealth() * 20)
+		monster:setHealth(monster:getMaxHealth())
 	else
 		player:sendCancelMessage("There is not enough room.")
 		position:sendMagicEffect(CONST_ME_POFF)
